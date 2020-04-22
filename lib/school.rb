fdf13aef0a33e33349b3a5d4f0c4089d6076023b
jsymondst <jammie@Jammie-XPS.localdomain>
2020-04-22 15:53:05 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+
+class School
+    
+    def initialize(name)
+        @name = name
+        @roster ={}
+    end
+
+    attr_reader :name, :roster
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        # @roster.each{|grade, students| students.sort!}
+        @roster.map{|grade, students| [grade, students.sort]}.to_h
+    end
+
+
+
+end
